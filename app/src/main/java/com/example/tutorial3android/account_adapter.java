ac57ee0e235d04960610544f254ca25af8e3fd74
alvinyap03 <yapkahseng2003@gmail.com>
2024-01-08 14:35:08 +0800

    commit

--- app/src/main/java/com/example/tutorial3android/account_adapter.java
+++ app/src/main/java/com/example/tutorial3android/account_adapter.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class account_adapter extends ArrayAdapter<String> {
 
+
     public account_adapter(Context context, int resource, List<String> usernames) {
         super(context, resource, usernames);
     }
